5cdff1f3b76217bf816f793bc7011e34aba43050
Shungo-I <iririn199810710@gmail.com>
2026-02-21 00:06:04 +0900

    feat: Add new SVG icons for the review copy and toolbar buttons, and configure Vite environment types.

--- src/features/reviewCopyButton/index.ts
+++ src/features/reviewCopyButton/index.ts
@@ -1,13 +1,9 @@
 import type { Feature } from "../../types";
+import clipboardIcon from "./icons/clipboard.svg?raw";
+import checkIcon from "./icons/check.svg?raw";
 
 const FEATURE_ID = "review-copy-button";
 const PROCESSED_ATTR = "data-gh-gearbox-copy-btn";
-
-/** クリップボードアイコン (SVG) */
-const CLIPBOARD_ICON = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor"><path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path><path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path></svg>`;
-
-/** チェックマークアイコン (SVG) */
-const CHECK_ICON = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor"><path d="M13.78 4.22a.75.75 0 0 1 0 1.06l-7.25 7.25a.75.75 0 0 1-1.06 0L2.22 9.28a.751.751 0 0 1 .018-1.042.751.751 0 0 1 1.042-.018L6 10.94l6.72-6.72a.75.75 0 0 1 1.06 0Z"></path></svg>`;
 
 let observer: MutationObserver | null = null;
 
@@ -177,7 +173,7 @@ const createCopyButton = (commentElement: HTMLElement): HTMLButtonElement => {
   button.className = "gh-gearbox-copy-btn";
   button.type = "button";
   button.title = "レビューコメントをコピー";
-  button.innerHTML = CLIPBOARD_ICON;
+  button.innerHTML = clipboardIcon;
 
   button.addEventListener("click", async (e) => {
     e.preventDefault();
@@ -193,11 +189,11 @@ const createCopyButton = (commentElement: HTMLElement): HTMLButtonElement => {
       await navigator.clipboard.writeText(copyText);
 
       // 成功フィードバック: チェックマークアイコンに変更
-      button.innerHTML = CHECK_ICON;
+      button.innerHTML = checkIcon;
       button.classList.add("gh-gearbox-copy-btn--success");
 
       setTimeout(() => {
-        button.innerHTML = CLIPBOARD_ICON;
+        button.innerHTML = clipboardIcon;
         button.classList.remove("gh-gearbox-copy-btn--success");
       }, 1500);
     } catch (error) {
